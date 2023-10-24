2e014dcef5b33486ee9de5b170cc3fdfe6991d9e
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-10-24 14:00:39 -0300

    base dir set do /

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	site: 'https://davivilarinho.github.io',
-//  base: '/',
+  base: '/',
 	integrations: [mdx(), sitemap()],
 });
