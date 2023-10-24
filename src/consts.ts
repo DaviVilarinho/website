f8d3af940ba342597a9d8fbe3cd3d15098f3f9b0
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-10-24 00:10:02 -0300

    mudanças parciais, incompleto

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'dev vilarinho';
+export const SITE_DESCRIPTION = 'eu sou o vilarinho, um dev, e este é meu site onde apresento projetos e posts';
